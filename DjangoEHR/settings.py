bd0bf34116a10ae2c7512f86fd8d6bc5e02dc977
yahyakhalaf99 <yahya.ahmed99@eng-st.cu.edu.eg>
2024-03-09 03:02:17 +0200

    go

--- DjangoEHR/settings.py
+++ DjangoEHR/settings.py
@@ -81,13 +81,23 @@ WSGI_APPLICATION = 'DjangoEHR.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'lab3database',
+        'PASSWORD': os.getinv("DB_PASSWORD"),
+        'HOST': 'lab3server.postgres.database.azure.com',
+        'PORT': '5432',
+        'OPTIONS':{'sslmode':'require'},
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
